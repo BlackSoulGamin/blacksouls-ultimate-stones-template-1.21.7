779a955ab9bf9793003bbd33c4d2378794e94807
BlackSoul <115299438+BlackSoulGamin@users.noreply.github.com>
2025-07-31 22:17:00 -0500

    Initial commit

---
+++ src/main/java/net/blacksoul/blacksoulsultimatestones/BlackSoulsUltimateStonesClient.java
@@ -0,0 +1,10 @@
+package net.blacksoul.blacksoulsultimatestones;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlackSoulsUltimateStonesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/blacksoul/blacksoulsultimatestones/BlackSoulsUltimateStonesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.blacksoul.blacksoulsultimatestones;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlackSoulsUltimateStonesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
